0ef5b49beccee31837effd7b03ef1389c7e72384
panda1920 <15102478+panda1920@users.noreply.github.com>
2018-12-04 00:50:48 +0900

    - Added color/style to test page - Now test page displays overall result of testRunner - Now there is a logic to decide whether to display/hide testResult

--- script/displaytesterhtml.js
+++ script/displaytesterhtml.js
@@ -2,22 +2,22 @@
 // output testResults to html
 //////////
 
+// runTest() related functions ------------------------------------
+
+// event fired when button is pressed
+function runTest(evt) {
+    runTestRunner();
+    createResultHtml();
+    createOverallResultHtml();
+    displayResult();
+}
 // run test cases
 function runTestRunner() {
     testRunner.run();
 }
-// empty element
-function emptyElement(element) {
-    element.textContent = "";
-}
-function createTestCaseHtml(result) {
-    let testcaseHTML = document.createElement("div");
-        testcaseHTML.textContent = result.getTestResultString();
-    return testcaseHTML;
-}
-// display result of each test case
-function displayResultOnHtml() {
-    let resultSpace = document.getElementById("testResult");
+// create html for testResult
+function createResultHtml() {
+    let resultSpace = findById("testResult");
     emptyElement(resultSpace);
     
     // make a div tag for each testcase and append to #testResult
@@ -25,13 +25,77 @@ function displayResultOnHtml() {
         resultSpace.appendChild(createTestCaseHtml(testCase.result));
     });
 }
-function runTest(evt) {
-    runTestRunner();
-    displayResultOnHtml();
+// create html for overall result
+function createOverallResultHtml() {
+    findById("testNums").textContent = createTestNumString(testRunner.testCases);
+    findById("totalTime").textContent = createTotalTimeString(testRunner.testCases);
+}
+// create text for test numbers
+function createTestNumString(testCases) {
+    let passCount = 0;
+    let failCount = 0;
+    testCases.forEach(testCase => {
+        testCase.result.testPassed ? passCount++ : failCount++;
+    });
+
+    return `Passed Tests: ${passCount}\r\nFailed Tests: ${failCount}`;
+}
+// create text for total time executed
+function createTotalTimeString(testCases) {
+    let totalTime = testCases.reduce(
+        (accum, testCase) => {
+            let executionTime = testCase.result.executionTime;
+            if (executionTime === null) excecutionTime = 0;
+            return accum + executionTime;
+        }, 0
+    );
+
+    return `Total Execution time: ${totalTime}ms`;
+}
+// display the result
+function displayResult() {
+    let resultSpace = findById("testResult");
+    let numTestCase = testRunner.testCases.length;
+    
+    if (numTestCase === 0)
+        resultSpace.style.display = "none;"
+    else
+        resultSpace.style.display = "block";
+}
+// empty element
+function emptyElement(element) {
+    element.textContent = "";
+}
+// create html element for testcase result
+function createTestCaseHtml(result) {
+    let text = result.getTestResultString();
+    return createDiv(text, null);
 }
 
+// Utility functions ------------------------------------
+
+// creates a div element with attribute of choice
+function createDiv(text, attributes) {
+    let divElement = document.createElement("div");
+        divElement.textContent = text;
+
+    for (let property in attributes) {
+        divElement.setAttribute(property, attributes[property]);
+    }
+    return divElement;
+}
+
+// find element by id
+function findById(idString) {
+    return document.getElementById(idString);
+}
+
+
+function initializePage() {
+    addEvents();
+}
 function addEvents() {
     let myButton = document.getElementById("startTest");
     myButton.addEventListener("click", runTest);
 }
-document.addEventListener("DOMContentLoaded", addEvents);
+document.addEventListener("DOMContentLoaded", initializePage);
